f45691fea2021f96fe54735c510de1fa6d254248
Tommy Waalkes <Tommy@grandcircus.co>
2018-10-11 09:57:22 -0400

    Added in Foreach Loop

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            int length = 7;
+            int length = 8;
             int[] numbers = new int[length];
             numbers[0] = 10;
             numbers[1] = 23;
@@ -15,12 +15,21 @@ namespace Array
             numbers[4] = 202;
             numbers[5] = 30;
             numbers[6] = 12;
+            numbers[7] = 22;
             int sum = 0; 
             //Counting up
             for(int i = 0; i < numbers.Length; i++)
             {
                 Console.WriteLine("At index "+i+ " the value is "+numbers[i]);
                 sum += numbers[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Foreach Example");
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
             }
 
             Console.WriteLine("The sum of the all the numbers is " + sum);
